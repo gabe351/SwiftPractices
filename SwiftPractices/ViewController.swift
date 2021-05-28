71e708d97254d72d3eadca0eb27978105b8eed89
Gabriel Rosa <gabe.rosa@dialpad.com>
2021-05-28 09:58:25 -0300

    Initial commit

---
+++ SwiftPractices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPractices
+//
+//  Created by Gabriel Rosa on 28/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
